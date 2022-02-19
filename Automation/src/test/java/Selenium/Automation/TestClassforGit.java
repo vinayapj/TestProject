867a5eaeb716d7ef117bd4cb5efa53d30e4a4c04
vinay <vinay@DESKTOP-5D13A30>
2022-02-19 19:06:44 +0530

    four

--- Automation/src/test/java/Selenium/Automation/TestClassforGit.java
+++ Automation/src/test/java/Selenium/Automation/TestClassforGit.java
@@ -4,7 +4,7 @@ public class TestClassforGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("git  Second");
+System.out.println("git  Sec ond");
 	}
 
 }
