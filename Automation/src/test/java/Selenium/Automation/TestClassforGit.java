8695227be7efce467ee4cc0dd98cf2c23bd4e0f2
vinay <vinay@DESKTOP-5D13A30>
2022-02-19 18:54:47 +0530

    second commit

---
+++ Automation/src/test/java/Selenium/Automation/TestClassforGit.java
@@ -0,0 +1,10 @@
+package Selenium.Automation;
+
+public class TestClassforGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("gitSecond");
+	}
+
+}
